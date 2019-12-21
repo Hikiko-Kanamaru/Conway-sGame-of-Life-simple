1e062ddd88c2e95e48e92420c61e377db3486d0d
Hikiko-Kanamaru <RockDoorFox@gmail.com>
2019-12-21 09:58:45 +0900

    Initial Commit

---
+++ Conway'sGame of Life simple/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Conway'sGame of Life simple
+//
+//  Created by リノ on 2019/12/21.
+//  Copyright © 2019 hikiko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

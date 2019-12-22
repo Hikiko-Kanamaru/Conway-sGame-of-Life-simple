61df9f1bcd00bff8a14cf4f2caa7daaa565af2cb
Hikiko-Kanamaru <RockDoorFox@gmail.com>
2019-12-22 09:47:30 +0900

    qiita記事の編集し始めた段階

--- Conway'sGame of Life simple/main.swift
+++ Conway'sGame of Life simple/main.swift
@@ -25,7 +25,7 @@ func mapCreate(Xjiku x:Int,Yjiku y:Int,seisei s:()->Bool = {Bool.random()} ) -> 
     return map
 }
 
-lifeData = mapCreate(Xjiku: 10, Yjiku:50 )
+lifeData = mapCreate(Xjiku: 10, Yjiku:10 )
 //print(lifeData)
 
 //ブロック状に表示してくれる。
@@ -52,52 +52,59 @@ func lifeView(world w:[[Bool]]) {
 
 lifeView(world: lifeData)
 
-//1ターン進める
+//1ターン進める　今回は、生存条件を変更不可能にする。
 func nextLife(world w:[[Bool]]) -> [[Bool]] {
 
     //毎回読み込ませると時間がかかるので、定数として読み込ませる
     let xCount = w.count
     let yCount = w[0].count
     
-    //周辺の密度を保存する。端っこかどうかの計算をなくすために、一マスづつ前後に大きくしています。両側ぶんで２足します
+    //周辺の密度を保存する。型がIntのため、mapCreateを使わない。端っこかどうかの計算をなくすために、一マスづつ前後に大きくしています。両側ぶんで２足します
     var kamitudo:[[Int]] = Array(repeating:{Array(repeating: 0, count: yCount + 2)}(), count: xCount + 2)
     
+    
     //返値を保存する場所 生命は減っていく傾向にあるのでfalse始まり。
-    var nextWorld = Array(repeating: {Array(repeating: false, count: yCount)}(), count: xCount)
+    //    var nextWorld = Array(repeating: {Array(repeating: false, count: yCount)}(), count: xCount)
+    var nextWorld  = mapCreate(Xjiku: xCount, Yjiku: yCount, seisei: {false})
     
     //引数worldを読み込み過密状況を調査する
     for x in 0..<xCount {
         for y in 0..<yCount{
             //マスに生命が存在したら、周辺の過密度を上昇させる
             if w[x][y] == true{
-                //過密度を書き込むループ　ハードコード(直接書き込む事)したほうが早いが、読みづらいのでforループを使う
+                //過密度を書き込むループ 9方向に加算する
+                //　ハードコード(直接書き込む事)したほうが早いが、読みづらいのでforループを使う
                 for i in 0...2 {
                     for t in 0...2{
                         kamitudo[x+i][y+t] += 1
                     }
-                                }
+                }
                 //自分は隣接する個数に含まれないので、１減らす
                 kamitudo[x+1][y+1] -= 1
             }
         }
     }
     
-    // kamitudoに基づき生存判定をしていく
+    // kamitudo(過密度)に基づき生存判定をしていく
     for x in 1...xCount{
         for y in 1...yCount {
             switch kamitudo[x][y] {
+                //３なら誕生
             case 3 :
                 nextWorld[x-1][y-1] = true
+                //２なら、マスに生命がいれば生存させる
             case 2 :
                 if w [x-1][y-1] == true {
                     nextWorld[x-1][y-1] = true
                 }
+                //それ以外は、基礎値でfalseのまま
             default:
-                //xcodeのエラー抑止
+                //xcodeのエラー抑止　*defaultに何も設定しないとエラーが出ます。
                 {}()
             }
         }
     }
+    
     return nextWorld
 }
 
@@ -105,21 +112,22 @@ func nextLife(world w:[[Bool]]) -> [[Bool]] {
 
 lifeData = nextLife(world: lifeData)
 lifeView(world: lifeData)
-
+/*
 for _ in 0..<100{
     lifeData = nextLife(world: lifeData)
 }
 lifeView(world: lifeData)
-
+*/
 
 //特定のマスを指示してデータを操作する関数 worldは現在の状態、pointは編集する場所(X軸,Y軸)、sayouは、セルに行う操作　デフォルトは、反転
-func kaminNoTe(world w :inout [[Bool]],point p :(Int,Int),sayou s:(Bool)->Bool = {!$0}) {
+func kamiNoTe(world w :inout [[Bool]],point p :(Int,Int),sayou s:(Bool)->Bool = {!$0}) {
     w[p.0][p.1] = s(w[p.0][p.1])
 }
 
 
+//一番下の行を反転させる
 for i in 0..<lifeData[0].count {
-    kaminNoTe(world: &lifeData, point: (lifeData.count - 1,i))
+    kamiNoTe(world: &lifeData, point: (lifeData.count - 1,i))
 }
 
 
